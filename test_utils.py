639246683947dac7f379e5d80204cc99268a3571
olingo99 <engels.theo@skynet.be>
2021-02-09 13:31:01 +0100

    Add Travis configuration

--- test_utils.py
+++ test_utils.py
@@ -7,16 +7,13 @@ import utils
 
 class TestUtils(unittest.TestCase):
     def test_fact(self):
-        # À compléter...
-        pass
+        self.assertEqual(utils.fact(5),120)
     
     def test_roots(self):
-        # À compléter...
-        pass
+        self.assertEqual(utils.roots(1,5,4),(-1,-4))
     
     def test_integrate(self):
-        # À compléter...
-        pass
+        self.assertEqual(utils.integrate(x**3,0,2),4)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
